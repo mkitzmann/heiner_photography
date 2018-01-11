3a6f4df7fbd3195e38c137cb7bef75c071e5f6ae
Moritz Kitzmann <moritz@iMacMoritz.home>
2018-01-11 18:30:31 +0100

    Add a resonse to the controlleAdd a resonse to the controllerr

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Response;
+
 class HomeController
 {
 
     public function indexAction()
     {
+
+        return new Response('Hallo Welt');
     }
 }
