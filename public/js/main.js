75894290949050ef5c1bdcc053ae4edbfa553624
Moritz Kitzmann <moritz@iMacMoritz.home>
2018-02-12 18:40:20 +0100

    Added Slick Slideshow

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.gallery-container').slick({
+        accessibility: true,
+        infinite: true,
+        lazyLoad: 'progressive',
+        adaptiveHeight: false,
+        fade: true,
+        cssEase: 'ease-in-out',
+        speed: 2000,
+});
+})
