12191a196cff91ce24741d173acbe410d6b1aaca
moritzkitzmann <mkitzmann@gmail.com>
2018-03-14 16:33:29 +0100

    added project-overview admin

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -68,4 +68,40 @@ class ProjectController extends Controller
             'nextphoto' => $photo[1]));
     }
 
+    public function editProjects(Request $request, FileUploader $fileUploader)
+    {
+        $projectRepo = $this->getDoctrine()->getRepository(Project::class);
+        $projects = $projectRepo->findAll();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $project = new Project();
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            /** @var Symfony\Component\HttpFoundation\File\UploadedFile $file */
+            $file = $project->getThumbnail();
+            $directory = $this->getParameter('thumbnail_directory');
+            $fileName = $fileUploader->upload($file, $directory);
+            $project->setThumbnail($fileName);
+
+            $title = $project->getTitle();
+            $slugify = new Slugify();
+            $project->setSlug($slugify->slugify($title));
+
+            $em->persist($project);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('AdminProjectsRoute'));
+        }
+
+        return $this->render('admin/adminProjects.html.twig', [
+            'projects' => $projects,
+            'form' => $form->createView(),
+        ]);
+
+    }
+
 }
